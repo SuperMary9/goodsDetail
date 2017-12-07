751a837c7941b5e4ab90f5b8826a2413116432a9
mahongli2 <2522837683@qq.com>
2017-12-07 19:32:39 +0800

    12月7日工作内容

--- js/main-mali.js
+++ js/main-mali.js
@@ -12,21 +12,34 @@ $(function(){
         paginationBulletRender: function (swiper, index, className) {
             console.log()
             return '<span class="' + className + '">' + '<i class="sliber-tit sliber-tit'+(index+1)+'">'+ titArr[index] +'</i></span>';
-        },
-
-        // 网格分布
-        centeredSlides : true,
-    
-        // 前进后退按钮
-        // nextButton: '.swiper-button-next',
-        // prevButton: '.swiper-button-prev', 
+        }
     });
 
     // 加入购物车操作
     $(".cart-btn").on("click", function(){
-        console.log(1);
-        $(".p-layer").show();
+        $(".js-cart-layer").show();
     });
+    // 关闭弹层操作
+    $(".p-layer-content").on( "click", ".icon-close", function(){
+        $(this).closest(".js-cart-layer").hide();
+    });
+
+    // 购物规则选择
+    $(".p-spec").on("click", "a", function(e){
+        $(this).closest("dl").find(".p-tips").removeClass("active");
+        $(this).addClass("active");
+    })
+
+    // 产品属性
+    var data = {
+        data:[
+            {'name':'规格',value:['50g','100g','150g','200g','250g','300g']},
+            {'name':'颜色',value:['红色','白色','蓝色']},
+            {'name':'尺寸',value:['18寸','20寸','21寸','22寸','23寸','24寸']}
+        ]
+    }
+    var html = template('product-wrap-html', data);
+    $("#product-wrap").html(html);
 
 
 });
