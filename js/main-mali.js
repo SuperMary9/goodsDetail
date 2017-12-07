56e92c81d314a0a9f0ad1900f6a7b4e576886ced
mahongli2 <2522837683@qq.com>
2017-12-07 10:40:01 +0800

    适配js文件修改

--- js/main-mali.js
+++ js/main-mali.js
@@ -1,24 +1,32 @@
 $(function(){
+    // 轮播图相关
     var titArr = ['产品','感受','详情','搭配'];
-        var mySwiper = new Swiper ('.swiper-container', {
-            direction: 'horizontal',
-            loop: false,
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'horizontal',
+        loop: false,
 
-            // 分页器
-            pagination: '.swiper-pagination',
-            paginationClickable: true,
-            paginationType : 'bullets',
-            paginationBulletRender: function (swiper, index, className) {
-                console.log()
-                return '<span class="' + className + '">' + '<i class="sliber-tit sliber-tit'+(index+1)+'">'+ titArr[index] +'</i></span>';
-            },
+        // 分页器
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        paginationType : 'bullets',
+        paginationBulletRender: function (swiper, index, className) {
+            console.log()
+            return '<span class="' + className + '">' + '<i class="sliber-tit sliber-tit'+(index+1)+'">'+ titArr[index] +'</i></span>';
+        },
 
-            // 网格分布
-            centeredSlides : true,
-        
-            // 前进后退按钮
-            // nextButton: '.swiper-button-next',
-            // prevButton: '.swiper-button-prev',
-            
-        }) 
+        // 网格分布
+        centeredSlides : true,
+    
+        // 前进后退按钮
+        // nextButton: '.swiper-button-next',
+        // prevButton: '.swiper-button-prev', 
+    });
+
+    // 加入购物车操作
+    $(".cart-btn").on("click", function(){
+        console.log(1);
+        $(".p-layer").show();
+    });
+
+
 });
